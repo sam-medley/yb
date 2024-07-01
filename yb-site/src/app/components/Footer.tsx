2cd27b5d9b9d1d71b365d7e93f8cab493e38779a
Samuel Medley <69830609+sam-medley@users.noreply.github.com>
2024-07-01 20:59:26 +0000

    sam's commit

--- yb-site/src/app/components/Footer.tsx
+++ yb-site/src/app/components/Footer.tsx
@@ -2,9 +2,24 @@ import React from 'react';
 import styles from '../styles/Home.module.css';
 
 const Footer: React.FC = () => {
+
   return (
     <footer className={styles.footer}>
-      <p>&copy; 2023 yb. All rights reserved.</p>
+      <div className={styles.TextContainer}>
+        <div className={styles.verticalText}>
+          <p>INSERT YOUR TEXT HERE</p>
+        </div>
+        <div className={styles.verticalText2}>
+          <p>INSERT YOUR TEXT HERE</p>
+        </div>
+        <div className={styles.imageContainer}>
+          <img src="/path/to/your/image.jpg" alt="Insert Image" />
+        </div>
+      </div>
+      <div className={styles.diagonalText}>
+        <p>Unlock digital rarity.<br />Own NFT<br />masterpieces.</p>
+        <p className={styles.bodyText}>&copy; 2023 yb. All rights reserved.</p>
+      </div>
     </footer>
   );
 };
